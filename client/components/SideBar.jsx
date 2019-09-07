410f434285f25c1bb3541525f374765ba8212175
linh-pham-nz <linh.pham.nz@gmail.com>
2019-09-08 11:55:41 +1200

    cats from DB displaying as cards on Adopt page

--- client/components/SideBar.jsx
+++ client/components/SideBar.jsx
@@ -4,38 +4,26 @@ import { HashRouter as Router, Route, Link } from 'react-router-dom'
 
 import Adopt from './Adopt'
 import Rehome from './Rehome'
-import { getDbCats } from '../apiClient'
 
 class SideBar extends React.Component {
     constructor(props) {
         super(props)
     } 
     
-    displayCats() {
-        getDbCats().then(result => {
-            const dbCats = result.body;
-            console.log("sideBar", dbCats)
-            // this.setState({
-            //     cats: dbCats
-            // })
-        })
-    }
-
     render() {
-        console.log(this.displayCats)
         return (
             <Router>
                 <div className="container-fluid sideBar-container">
                     <nav className="navbar navbar-expand-lg navbar-light">
                         <div className="container">
                             <h3><Link to='/adopt' onClick={this.displayCats}>Adopt a cat</Link></h3>
-                            {/* <h3><Link to='/rehome'>Rehome a cat</Link></h3> */}
+                            <h3><Link to='/rehome'>Rehome a cat</Link></h3>
                         </div>
                     </nav>
                 </div>
 
                 <Route path="/Adopt" component={Adopt} />
-                {/* <Route path="/Rehome" component={Rehome} /> */}
+                <Route path="/Rehome" component={Rehome} />
             </Router>
         )
     }
