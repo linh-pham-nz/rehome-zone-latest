b5824ecc087c10ab01d71623e119fb178d67f402
linh-pham-nz <linh.pham.nz@gmail.com>
2019-09-08 12:16:11 +1200

    form displays on rehome page

--- client/components/Rehome.jsx
+++ client/components/Rehome.jsx
@@ -2,8 +2,39 @@ import React from 'react'
 
 const Rehome = () => {
     return (
-        <h2>Rehome a cat!</h2>
+        <React.Fragment>
+            <div className="container">
+                <h2>Rehome a cat!</h2>
+                <div>
+                    <form>
+                        <div>
+                            <label>Cat name: 
+                                <input type="text" name="name" />
+                            </label>
+                        </div>
+                        <div>
+                            <label>Cat breed:
+                            <input type="text" name="breed" />
+                            </label>
+                        </div>
+                        <div>
+                            <label>Cat age:
+                            <input type="text" name="age" />
+                            </label>
+                        </div>
+                        <input type="submit" value="Submit" />
+                    </form>
+                </div>
+            </div>
+        </React.Fragment>
     )
 }
 
 export default Rehome
+
+
+// <form>
+// <div>
+//     <input type='text' name='formName' >Cat's name: </input>    
+// </div>
+// </form>
