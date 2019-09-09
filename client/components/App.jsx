d537694ccccdae63709e07156b9856536edff45e
linh-pham-nz <linh.pham.nz@gmail.com>
2019-09-09 20:44:23 +1200

    refactor code, delete sideBar component

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,17 +1,14 @@
 import React from 'react'
 
-
-
 import Nav from './Nav'
-import SideBar from './SideBar'
 
 const App = () => {
   return (
-    <div className='app'>
-      <Nav />
-      <SideBar />
-
-    </div>
+    <React.Fragment>
+      <div>
+        <Nav />     
+      </div>
+    </React.Fragment>
     
   )
 }
--- client/components/Nav.jsx
+++ client/components/Nav.jsx
@@ -1,15 +1,30 @@
 import React from 'react'
 
-const Nav = () => {
+import { HashRouter as Router, Route, Link } from 'react-router-dom'
+
+import Adopt from './Adopt'
+import Rehome from './Rehome'
+
+const Nav = () => {    
     return (
+        <Router>
             <div className="navBar container-fluid">
                 <nav className="navbar navbar-expand-lg navbar-dark bg-dark navbar-fixed-top">
                     <div className="container">
                         <h1>Rehome Zone</h1>
+                        <h3><Link to='/adopt'>Adopt a cat</Link></h3>
+                        <h3><Link to='/rehome'>Rehome a cat</Link></h3>
                     </div>
                 </nav>
             </div>
+
+            <Route path="/Adopt" component={Adopt} />
+            <Route path="/Rehome" component={Rehome} />
+        </Router>
     )
 }
 
 export default Nav
+
+
+
