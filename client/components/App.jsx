b21b8962cbb009b440bd1e8be51e1ac3ee7f44b1
student <student@pop-os.localdomain>
2019-09-18 16:17:09 +1200

    cat facts component created, link opens from main page

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -6,6 +6,7 @@ import RehomeZone from './RehomeZone'
 import Adopt from './Adopt'
 import Rehome from './Rehome'
 import Name from './Name'
+import CatFacts from './CatFacts'
 
 const App = () => {
   return (
@@ -17,15 +18,17 @@ const App = () => {
             <ul className="nav navbar-nav">
               <li><Link to='/adopt'><i className="fa fa-paw"></i>Adopt a cat</Link></li>
               <li><Link to='/rehome'><i className="fa fa-paw"></i>Rehome a cat</Link></li>
+              <li><Link to='/cat-facts'><i className="fa fa-paw"></i>Cat facts</Link></li>
             </ul>
           </div>
         </nav>
       </div>
       
       <Route exact={true} path='/' component={RehomeZone} />
-      <Route path="/Adopt" component={Adopt} />
-      <Route path="/Name/:id" component={Name} />
-      <Route path="/Rehome" component={Rehome} />
+      <Route path="/adopt" component={Adopt} />
+      <Route path="/name/:id" component={Name} />
+      <Route path="/rehome" component={Rehome} />
+      <Route path="/cat-facts" component={CatFacts} />
     </Router>
   )
 }
---
+++ client/components/CatFacts.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const CatFacts = () => {
+    console.log("hello")
+    return (
+        <div className="container">
+            <h3 className="component-heading">CAT FACTS</h3>
+            <h3 className="main-text">Click on the button below for a random cat fact!</h3>
+        </div>
+    )
+}
+
+export default CatFacts
