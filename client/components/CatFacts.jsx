ec874506b71a834ad6a70e05d52ad0532473eaae
student <student@pop-os.localdomain>
2019-09-18 16:44:00 +1200

    change CatFacts to class-based component

--- client/components/CatFacts.jsx
+++ client/components/CatFacts.jsx
@@ -1,14 +1,17 @@
 import React from 'react'
+import { connect } from 'react-redux'
 
-const CatFacts = () => {
-    console.log("hello")
-    return (
-        <div className="container">
-            <h3 className="component-heading">CAT FACTS</h3>
-            <h3 className="main-text">Click on the button below for a random cat fact!</h3>
-            <button className="meow-button">Meow!!!</button>
-        </div>
-    )
+class CatFacts extends React.Component {
+    render() {
+        console.log("hello")
+        return (
+            <div className="container">
+                <h3 className="component-heading">CAT FACTS</h3>
+                <h3 className="main-text">Click on the button below for a random cat fact!</h3>
+                <button className="meow-button">Meow!!!</button>
+            </div>
+        )
+    }
 }
 
-export default CatFacts
+export default connect()(CatFacts)
