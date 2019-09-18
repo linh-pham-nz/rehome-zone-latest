a5a453f9f0713357deb041f32a7b39fb8caf56e0
student <student@pop-os.localdomain>
2019-09-18 17:57:08 +1200

    roll back some code for pull request

--- client/components/CatFacts.jsx
+++ client/components/CatFacts.jsx
@@ -1,17 +1,13 @@
 import React from 'react'
-import { connect } from 'react-redux'
 
-class CatFacts extends React.Component {
-    render() {
-        console.log("hello")
-        return (
-            <div className="container">
-                <h3 className="component-heading">CAT FACTS</h3>
-                <h3 className="main-text">Click on the button below for a random cat fact!</h3>
-                <button className="meow-button">Meow!!!</button>
-            </div>
-        )
-    }
+const CatFacts = () => {
+    return (
+        <div className="container">
+            <h3 className="component-heading">CAT FACTS</h3>
+            <h3 className="main-text">Click on the button below for a random cat fact!</h3>
+            <button className="meow-button">Meow!!!</button>
+        </div>
+    )
 }
 
-export default connect()(CatFacts)
+export default CatFacts
