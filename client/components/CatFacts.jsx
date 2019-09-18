cb86d4504be88fb1c7cfa216da10f40cb111971c
student <student@pop-os.localdomain>
2019-09-18 16:30:03 +1200

    meow button created with some styling

--- client/components/CatFacts.jsx
+++ client/components/CatFacts.jsx
@@ -6,6 +6,7 @@ const CatFacts = () => {
         <div className="container">
             <h3 className="component-heading">CAT FACTS</h3>
             <h3 className="main-text">Click on the button below for a random cat fact!</h3>
+            <button className="meow-button">Meow!!!</button>
         </div>
     )
 }
